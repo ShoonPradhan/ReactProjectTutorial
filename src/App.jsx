4143c705a79070bf9b373b76b32c1ca6c664515d
ShoonPradhan <shoonpradhan@gmail.com>
2025-11-05 15:34:37 +0630

    SSS

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { Button } from "./component/Button";
+
+export default function App() {
+
+   const handleClick = () => {
+    alert('Button clicked!');
+  };
+
+
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+      <h1 className="text-red-600 text-4xl font-bold">
+        Tailwind Works No! 🎉
+      </h1>
+     <Button color="green" onClick={handleClick}>Click Me</Button>
+      <Button color="red" onClick={handleClick}>Delete</Button>
+    </div>
+  );
+}
