72b2b26f826534b88562e0bc742b0fe6ba74c018
ShoonPradhan <shoonpradhan@gmail.com>
2025-11-05 15:36:44 +0630

    ss

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ export default function App() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
       <h1 className="text-red-600 text-4xl font-bold">
-        Tailwind Works No! 🎉
+        Tailwind Works! 🎉
       </h1>
      <Button color="green" onClick={handleClick}>Click Me</Button>
       <Button color="red" onClick={handleClick}>Delete</Button>
